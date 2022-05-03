94782e412cf3c297b4ccf6b3c2ae6428f6b03486
Reponse Mod <cinarbenliiphone@hotmail.com>
2022-05-04 01:15:15 +0300

    Proje dosyası ekle.

---
+++ ReponseMod_AdvancedİnfinityEnergy/Class1.cs
@@ -0,0 +1,36 @@
+﻿using Rocket.API;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReponseMod_AdvancedİnfinityEnergy
+{
+    public class Class1 : RocketPlugin<Config>
+    {
+        protected override void Load()
+        {
+            base.Load();
+            UnturnedPlayerEvents.OnPlayerUpdateStamina += Energy;
+        }
+
+        private void Energy(UnturnedPlayer player, byte stamina)
+        {
+            if (!player.HasPermission(Configuration.Instance.VipPerm))
+            {
+                return;
+            }
+
+            if (stamina >= 50)
+            {
+                return;
+            }
+
+            player.Player.life.serverModifyStamina(100);
+        }
+    }
+}
---
+++ ReponseMod_AdvancedİnfinityEnergy/Config.cs
@@ -0,0 +1,13 @@
+﻿using Rocket.API;
+
+namespace ReponseMod_AdvancedİnfinityEnergy
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public string VipPerm;
+        public void LoadDefaults()
+        {
+            VipPerm = "Vip";
+        }
+    }
+}
